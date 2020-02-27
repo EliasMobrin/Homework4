963e29563c545650c815d1379d51e245095e15fd
Elias Mobrin <l.mobrin@setonhill.edu>
2020-02-26 20:30:34 -0500

    Ready to be reviewed

--- Homework 4/ViewController.swift
+++ Homework 4/ViewController.swift
@@ -9,6 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBAction func displayAlert1(_ sender: Any) {
+        // Setup the alert
+        let alert = UIAlertController(title: "Important Message", message: "Don't press the button in the bottom right corner.", preferredStyle: .alert)
+        
+        // Add the action
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment:  "Default Action"), style: .`default`, handler: {_ in}))
+        
+        // Display the alert
+        self.present(alert,animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func displayAlert2(_ sender: Any) {
+        // Setup the alert
+        let alert = UIAlertController(title: "Be Careful", message: "Whatever you do, DONT PRESS IT!", preferredStyle: .alert)
+        
+        // Add the action
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment:  "Default Action"), style: .`default`, handler: {_ in}))
+        
+        // Display the alert
+        self.present(alert,animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func displayAlert3(_ sender: Any) {
+        // Setup the alert
+        let alert = UIAlertController(title: "Listen Up", message: "Trust me it is not worth it.", preferredStyle: .alert)
+        
+        // Add the action
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment:  "Default Action"), style: .`default`, handler: {_ in}))
+        
+        // Display the alert
+        self.present(alert,animated: true, completion: nil)
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
